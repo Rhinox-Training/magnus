c7c70528800306b0c3a5dd9e8378ca29f8729e11
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-04-20 11:59:43 +0200

    [ADD] Added commands to remove component(s) on a gameobject and to get all components on the object itself

---
+++ Assets/Magnus/Scripts/CommandSystem/Commands/Unity/GameObject/GetComponentsOnGameObjectCommand.cs
@@ -0,0 +1,22 @@
+﻿using Rhinox.Lightspeed;
+using UnityEngine;
+
+namespace Rhinox.Magnus.CommandSystem
+{
+    [CommandInfo("Get all components on a GameObject and (if desired) its children", "GameObject")]
+    public class GetComponentsOnGameObjectCommand : BaseGameObjectConsoleCommand
+    {
+        public override string CommandName => "get-components";
+
+        protected override string[] ExecuteFor(GameObject go, string[] args)
+        {
+            var components = go.GetComponents<Component>();
+            string resultString = $"The following components were found on {go.name}: ";
+            foreach (Component component in components)
+            {
+                resultString += component.GetType().Name + " ";
+            }
+            return new[] { resultString };
+        }
+    }
+}
