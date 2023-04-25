959141f685a9b850b9a662682f859c61d0af7d10
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-04-25 08:48:11 +0200

    [ADD] Target Frame Rate and VSync Count commands

---
+++ Scripts/CommandSystem/Commands/Unity/Application Settings/VSyncCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Rhinox.Lightspeed;
+using UnityEngine;
+
+namespace Rhinox.Magnus.CommandSystem
+{
+    [CommandInfo("Sets the vsync count", "Application Settings")]
+    public class VSyncCount: IConsoleCommand
+    {
+        public string CommandName => "vsync-count";
+        public string[] Execute(string[] args)
+        {
+            if (args.IsNullOrEmpty())
+                return new[] { $"The current vsync count is: {QualitySettings.vSyncCount}" };
+            
+            if (!int.TryParse(args.First(), out int count))
+                return new[] { $"Was unable to parse {args.First()} to an integer" };
+            
+            QualitySettings.vSyncCount = count;
+            return new[] { $"The vsync count is now: {QualitySettings.vSyncCount}" };
+        }
+    }
+}
