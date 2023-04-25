6cfebc0d1a93e7439e9ac363ab626c1455f70fe7
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-04-25 10:38:25 +0200

    [ADD] Added full screen commands and gravity commands

---
+++ Scripts/CommandSystem/Commands/Unity/Screen/FullScreenModeCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Rhinox.Lightspeed;
+using UnityEngine;
+
+namespace Rhinox.Magnus.CommandSystem
+{
+    [CommandInfo("Sets the full screen mode", "Screen")]
+    public class FullScreenModeCommand: IConsoleCommand
+    {
+        public string CommandName => "full-screen-mode";
+        public string[] Execute(string[] args)
+        {
+            if(args.IsNullOrEmpty())
+                return new [] { "Full screen mode is " + (Screen.fullScreenMode) };
+            
+            if(!Enum.TryParse(args.First(), out FullScreenMode mode))
+                return new [] { $"Unable to parse FullScreenMode from {args.First()}" };
+            
+            Screen.fullScreenMode = mode;
+            return new [] { $"Full screen mode set to {mode}" };
+        }
+    }
+}
---
+++ Scripts/CommandSystem/Commands/Unity/Screen/ToggleFullScreenCommand.cs
@@ -0,0 +1,20 @@
+﻿using Rhinox.Lightspeed;
+using UnityEngine;
+
+namespace Rhinox.Magnus.CommandSystem
+{
+    [CommandInfo("Toggle Full Screen", "Screen")]
+    public class ToggleFullScreenCommand : IConsoleCommand
+    {
+        public string CommandName => "toggle-full-screen";
+
+        public string[] Execute(string[] args)
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+            if (Screen.fullScreen)
+                return new[] { "Changed to full screen." };
+            else
+                return new[] { "Changed to windowed." };
+        }
+    }
+}
