7edb11a38a195778226818b728ead88b78d189fd
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-04-27 11:30:59 +0200

    [ADD] Added PlayerCommands and Level command
    ### Added
    - get-active-level: Gets the path of the current level
    - kill-player: Kills the loaded player
    - respawn-player: Respawns the player (optional parameters are Vector3 position, Vector3 rotation, bool persistent
    - String parser for Quaternion and Vector3

---
+++ Scripts/CommandSystem/Commands/Level/GetActiveLevelCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Rhinox.Magnus.CommandSystem
+{
+    [CommandInfo("Gets the path of the current level", "Level")]
+    public class GetActiveLevelCommand : IConsoleCommand
+    {
+        public string CommandName => "get-active-level";
+        public string Syntax => "get-active-level";
+
+        public string[] Execute(string[] args)
+        {
+            return new[] { LevelLoader.GetActiveScene() };
+        }
+    }
+}
---
+++ Scripts/CommandSystem/Commands/Player/KillPlayerCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Rhinox.Magnus.CommandSystem.Player
+{
+    [CommandInfo("Kills the loaded player", "Player")]
+    public class KillPlayerCommand : IConsoleCommand
+    {
+        public string CommandName => "kill-player";
+        public string Syntax => "kill-player";
+
+        public string[] Execute(string[] args)
+        {
+            PlayerManager.Instance.KillPlayer();
+            return new[] { "Player killed." };
+        }
+    }
+}
