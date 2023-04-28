ab2a1b8b7908537cb424f704bd98ff2682b90466
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-04-28 11:31:30 +0200

    [ADD] Added the graphy package and toggle-graphy command
    ### Added
    - Optional graphy command

---
+++ Scripts/CommandSystem/Commands/Optional/ToggleGraphyCommand.cs
@@ -0,0 +1,23 @@
+﻿
+#if USING_GRAPHY
+namespace Rhinox.Magnus.CommandSystem
+{
+    [CommandInfo("Toggles the Graphy counter", "FPS")]
+    public class ToggleGraphyCommand : IConsoleCommand
+    {
+        public string CommandName => "toggle-graphy";
+        public string Syntax => CommandName;
+
+        public string[] Execute(string[] args)
+        {
+            if (GraphyService.Instance == null)
+            {
+                return new[] { "Graphy service not loaded." };
+            }
+            
+            GraphyService.Instance.ToggleGUI();
+            return new[] { "Graphy counters toggled." };
+        }
+    }
+}
+#endif
---
+++ Scripts/Services/GraphyService.cs
@@ -0,0 +1,38 @@
+﻿#if USING_GRAPHY
+using System.Reflection;
+using Rhinox.Perceptor;
+using Tayx.Graphy;
+using UnityEngine;
+
+namespace Rhinox.Magnus
+{
+    [ServiceLoader()]
+    public class GraphyService : AutoService<GraphyService>
+    {
+        private GameObject _graphyInstance;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            _graphyInstance = GameObject.Instantiate(MagnusProjectSettings.Instance.GraphyPrefab, transform, false);
+
+            var startupField = typeof(GraphyManager).GetField("m_enableOnStartup",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            startupField.SetValue(GraphyManager.Instance, false);
+            
+        }
+
+        public void ToggleGUI()
+        {
+            if (_graphyInstance == null)
+            {
+                PLog.Warn<MagnusLogger>("Graphy instance is null");
+                return;
+            }
+
+            GraphyManager.Instance.ToggleActive();
+        }
+    }
+}
+#endif
