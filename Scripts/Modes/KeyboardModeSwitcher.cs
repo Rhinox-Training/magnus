f463799974fe7b44af57cfac2f8bf7a9cc1d267e
Gaetan Deglorie <gaetan@rhinox.training>
2023-08-04 13:06:15 +0200

    [ADD] Keyboard GameMode Switcher

---
+++ Scripts/Modes/KeyboardModeSwitcher.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Rhinox.Magnus;
+using Rhinox.Perceptor;
+using Rhinox.Utilities;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Rhinox.Magnus
+{
+    [Serializable]
+    public class EasyGameModeBind
+    {
+        public KeyCode KeyCode;
+        public GameMode Mode;
+    }
+    
+    public class KeyboardModeSwitcher : SerializedMonoBehaviour
+    {
+        public List<EasyGameModeBind> ModeBinds = new List<EasyGameModeBind>();
+        private float _lastPressTime;
+        [Range(0.1f, 3.0f), Tooltip("In seconds")]
+        public float SpamPreventionWindow = 0.5f;
+
+        public Audio SwitchingAudio;
+
+        private void Awake()
+        {
+            if (ModeBinds == null)
+                ModeBinds = new List<EasyGameModeBind>();
+
+            foreach (var modebind in ModeBinds)
+                GameModeManager.Instance.Register(modebind.Mode);
+        }
+        
+        private void Update()
+        {
+            
+
+            foreach (var modebind in ModeBinds)
+            {
+                if (Input.GetKeyDown(modebind.KeyCode))
+                {
+                    if ((Time.realtimeSinceStartup - _lastPressTime) <= SpamPreventionWindow)
+                    {
+                        PLog.Warn<MagnusLogger>("Prevented spam of GameMode Switching");
+                        return;
+                    }
+                    
+                    PLog.TraceDetailed<MagnusLogger>($"Received keyboard bind ModeSwitch");
+                    GameModeManager.Instance.SwitchTo(modebind.Mode.Name);
+                    if (AudioManager.HasInstance && SwitchingAudio != null && SwitchingAudio.Clip != null)
+                        AudioManager.Instance.PlayOneShot(SwitchingAudio.Clip, SwitchingAudio.Volume);
+                    _lastPressTime = Time.realtimeSinceStartup;
+                    break;
+                }
+            }
+        }
+    }
+}
