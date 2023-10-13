9ca39df7269adb2af67887172e6affc693b15552
Gaetan Deglorie <gaetan@rhinox.training>
2023-10-13 14:09:39 +0200

    [ADD] PlayerSubsystem architecture

--- Assets/Magnus/Scripts/PlayerManagement/Config/PlayerConfig.cs
+++ Assets/Magnus/Scripts/PlayerManagement/Config/PlayerConfig.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Rhinox.Utilities;
 using UnityEngine;
 
@@ -5,12 +6,23 @@ namespace Rhinox.Magnus
 {
     public abstract class PlayerConfig : ScriptableObject
     {
+        public List<IPlayerSubsystem> PlayerSubsystems;
+        
         public virtual Player Load(PlayerProfile profile, Transform parent)
         {
             Player p = CreatePlayer(parent, profile);
             if (p == null)
                 return null;
             p.LoadedConfig = this;
+            if (PlayerSubsystems != null)
+            {
+                foreach (var subSystem in PlayerSubsystems)
+                {
+                    if (subSystem == null)
+                        continue;
+                    subSystem.Initialize(p);
+                }
+            }
             return p;
         }
 
@@ -18,7 +30,15 @@ namespace Rhinox.Magnus
 
         public virtual void OnKillPlayer()
         {
-            
+            if (PlayerSubsystems != null)
+            {
+                foreach (var subSystem in PlayerSubsystems)
+                {
+                    if (subSystem == null)
+                        continue;
+                    subSystem.Terminate();
+                }
+            }
         }
     }
 }
