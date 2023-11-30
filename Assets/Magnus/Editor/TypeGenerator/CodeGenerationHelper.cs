d9116bce9cd7ade3c53a03be263084b306bd8701
Gaetan Deglorie <gaetan@rhinox.training>
2023-11-30 12:54:18 +0100

    [ADD] UnitySafeActivator: Generate generic class instances that are serializable by Unity
     - Factory pattern to generate objects, can be set and generated from Magnus Project Settings
    [ADD] Code generation tools to generate custom classes (MetaProgramming)

---
+++ Assets/Magnus/Editor/TypeGenerator/CodeGenerationHelper.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Rhinox.Lightspeed;
+using Rhinox.Lightspeed.Reflection;
+
+namespace Rhinox.Magnus.Editor.TypeGenerator
+{
+    public class CodeGenerationHelper
+    {
+        private List<string> _lines;
+
+        private const string REAL_TAB = "    ";
+
+        private int _curIndent = 0;
+
+        private enum GeneratorContext
+        {
+            FILE = 0,
+            NAMESPACE,
+            CLASS,
+            METHOD
+        }
+
+        private GeneratorContext _context;
+        private MethodBodyHelper _currentBodyHelper;
+
+        public CodeGenerationHelper()
+        {
+            _lines = new List<string>();
+            _curIndent = 0;
+            _context = GeneratorContext.FILE;
+        }
+        
+        public void CreateNamespace(string namespaceName)
+        {
+            if (_context > GeneratorContext.FILE)
+                CloseNamespace();
+            
+            _lines.Add($"{GetIndent()}namespace {namespaceName.Trim()}");
+            _lines.Add($"{GetIndent()}{{");
+
+            ++_curIndent;
+            _context = GeneratorContext.NAMESPACE;
+        }
+
+        public void CloseNamespace()
+        {
+            if (_context > GeneratorContext.NAMESPACE)
+                CloseClass();
+            
+            _lines.Add($"{GetIndent()}}}");
+            --_curIndent;
+            _context = GeneratorContext.FILE;
+        }
+
+
+        public void OpenClass(string className, bool isStatic)
+        {
+            OpenClass(className, isStatic, Array.Empty<string>());
+        }
+        
+        public void OpenClass(string className, bool isStatic, params Type[] baseTypes)
+        {
+            OpenClass(className, isStatic, baseTypes.Select(x => x.FullName).ToArray());
+        }
+
+        public void OpenClass(string className, bool isStatic, params string[] baseTypes)
+        {
+            
+            _lines.Add($"{GetIndent()}public {(isStatic ? "static " : "")}class {className.Trim()}{(baseTypes.Length > 0 ? " : " + string.Join(", ", baseTypes) : "")}");
+            _lines.Add($"{GetIndent()}{{");
+            
+            ++_curIndent;
+            _context = GeneratorContext.CLASS;
+        }
+        
+        public void CloseClass()
+        {
+            
+            if (_context > GeneratorContext.CLASS)
+                CloseMethod();
+            
+            _lines.Add($"{GetIndent()}}}");
+            --_curIndent;
+            _context = GeneratorContext.NAMESPACE;
+        }
+
+        public IImplementationBodyHelper OpenMethod(string methodName, bool isStatic, bool isPublic, Type[] arguments,
+            string[] argNames, Type returnType = null, bool shouldWriteOverride = false)
+        {
+            if (_context == GeneratorContext.METHOD)
+                CloseMethod();
+
+            if (isStatic)
+                shouldWriteOverride = false;
+            
+            string signature = $"{(isPublic ? "public" : "private")} {(isStatic ? "static " : "")}{(shouldWriteOverride ? "override " : "")}{(returnType != null && returnType != typeof(void) ? returnType.GetCSharpName() : "void")} {methodName.Trim()}(";
+            for (int i = 0; i < arguments.Length; ++i)
+            {
+                signature += arguments[i].GetCSharpName() + " " + argNames[i];
+                if (i < arguments.Length - 1)
+                    signature += ", ";
+            }
+
+            signature += ")";
+            
+            AddRaw(signature);
+            AddRaw("{");
+            ++_curIndent;
+            _context = GeneratorContext.METHOD;
+
+            _currentBodyHelper = new MethodBodyHelper(this);
+            return _currentBodyHelper;
+        }
+
+        public IImplementationBodyHelper OpenMethod(MethodInfo methodInfo)
+        {
+            bool shouldWriteOverride = !methodInfo.DeclaringType.IsInterface && (methodInfo.IsAbstract || methodInfo.IsVirtual);
+            return OpenMethod(methodInfo.Name, methodInfo.IsStatic, methodInfo.IsPublic, methodInfo.GetParameters(), methodInfo.ReturnType, shouldWriteOverride: shouldWriteOverride);
+        }
+
+        public IImplementationBodyHelper OpenMethod(string methodName, bool isStatic, bool isPublic,
+            ParameterInfo[] getParameters, Type returnType = null, bool shouldWriteOverride = false)
+        {
+            return OpenMethod(methodName, isStatic, isPublic, getParameters.Select(x => x.ParameterType).ToArray(),
+                getParameters.Select(x => x.Name).ToArray(), returnType, shouldWriteOverride);
+        }
+
+        private void CloseMethod()
+        {
+            if (_context != GeneratorContext.METHOD)
+                return;
+            
+            if (_currentBodyHelper != null)
+                _currentBodyHelper.Close();
+            _currentBodyHelper = null;
+            --_curIndent;
+            _context = GeneratorContext.CLASS;
+        }
+
+        private string GetIndent()
+        {
+            if (_curIndent <= 0)
+                return String.Empty;
+            return string.Join(string.Empty, Enumerable.Repeat(REAL_TAB, _curIndent));
+        }
+
+        public void AddRaw(string line)
+        {
+            _lines.Add($"{GetIndent()}{line}");
+        }
+
+        public void AddNewLine()
+        {
+            AddRaw(string.Empty);
+        }
+
+        public ICollection<string> ToLines()
+        {
+            if (_context >= GeneratorContext.CLASS)
+                CloseNamespace();
+
+            return _lines;
+        }
+        
+        public class MethodBodyHelper : IImplementationBodyHelper
+        {
+            private readonly CodeGenerationHelper _root;
+            private readonly MethodBodyHelper _parent;
+            private List<MethodBodyHelper> _children;
+            private bool _isClosed;
+
+            private enum BodyType
+            {
+                If,
+                // For
+            }
+
+            private BodyType _type;
+
+            public MethodBodyHelper(CodeGenerationHelper root)
+            {
+                _root = root;
+                _children = new List<MethodBodyHelper>();
+            }
+
+            public MethodBodyHelper(MethodBodyHelper currentBodyHelper)
+            {
+                _parent = currentBodyHelper;
+                if (_parent != null)
+                    _parent._children.Add(this);
+                _root = currentBodyHelper._root;
+            }
+
+            public void AddRaw(string s)
+            {                
+                TryCloseLastActiveChild();
+                _root.AddRaw(s);
+            }
+
+            public IImplementationBodyHelper CreateIfStatement(string s, bool stitchInIfElseBlock = false)
+            {
+                TryCloseLastActiveChild();
+                // if (!_isClosed)
+                //     Close();
+
+                string ifStatement;
+                if (stitchInIfElseBlock && HasPrecedingIf())
+                    ifStatement = $"else if ({s})";
+                else
+                    ifStatement = $"if ({s})";
+                AddRaw(ifStatement);
+                AddRaw("{");
+
+                ++_root._curIndent;
+                _type = BodyType.If;
+                
+                var childHelper = new MethodBodyHelper(this);
+                return childHelper;
+            }
+
+            private void TryCloseLastActiveChild()
+            {
+                if (_children != null)
+                {
+                    var child = _children.LastOrDefault();
+                    if (child != null && !child._isClosed)
+                        child.Close();
+                }
+            }
+
+            private bool HasPrecedingIf()
+            {
+                if (_children.IsNullOrEmpty())
+                    return false;
+
+                var lastChild = _children.LastOrDefault();
+                if (lastChild == null)
+                    return false;
+
+                return lastChild._type == BodyType.If;
+            }
+
+            // public IImplementationBodyHelper CreateForLoop(string indexer, string collection, bool isArray = true)
+            // {
+            //     throw new NotImplementedException();
+            // }
+
+            public void Close()
+            {
+                if (_isClosed)
+                    return;
+                
+                if (_children != null)
+                {
+                    foreach (var child in _children)
+                        child.Close();
+                }
+
+                --_root._curIndent;
+                AddRaw("}");
+                _isClosed = true;
+            }
+        }
+    }
+
+    public interface IImplementationBodyHelper
+    {
+        void AddRaw(string s);
+        IImplementationBodyHelper CreateIfStatement(string s, bool stitchInIfElseBlock = false);
+        // IImplementationBodyHelper CreateForLoop(string indexer, string collection, bool isArray = true);
+    }
+}
---
+++ Assets/Magnus/Editor/UnitySafeTypeGenerationSettingsDrawer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Rhinox.GUIUtils.Editor;
+using Rhinox.Lightspeed;
+using Rhinox.Lightspeed.Reflection;
+using Rhinox.Perceptor;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rhinox.Magnus.Editor
+{
+    [CustomPropertyDrawer(typeof(UnitySafeTypeGenerationSettings))]
+    public class UnitySafeTypeGenerationSettingsDrawer : BasePropertyDrawer<UnitySafeTypeGenerationSettings>
+    {
+        private Regex _classNameValidator;
+        
+        protected override void DrawProperty(Rect position, ref GenericHostInfo data, GUIContent label)
+        {
+            var nextRect = CallInnerDrawer(position, label);
+
+            if (GUI.Button(nextRect, "Generate Factory Class"))
+                TryGenerateTypeFactoryClass();
+            
+            if (GUI.Button(nextRect.MoveDownLine(), "Auto-populate Factories"))
+                SmartValue.PopulateFromCode();
+        }
+        
+        private void TryGenerateTypeFactoryClass()
+        {
+            EditorInputDialog.Create("Metaprogramming Helper", "Generate a factory class for an open generic type...")
+                .TextField("Class Name", out var classNameField, validation: ValidateClassName)
+                .Dropdown("Open Generic Type", GetGenericTypeOptions(), out var genericTypeField)
+                .OpenFolderField("Class Save Location", out var saveLocation)
+                .OnAccept(() =>
+                {
+                    var genericTypeDefinition = genericTypeField.Value.Value as Type;
+                    if (genericTypeDefinition == null || !genericTypeDefinition.IsGenericTypeDefinition)
+                    {
+                        PLog.Error<MagnusLogger>($"Something went wrong, type '{genericTypeDefinition?.GetCSharpName()}' was not an open generic type...");
+                        return;
+                    }
+                    
+                    var typeOptions = UnityGenericImplementationGenerator.GetDefaultTypeOptions().ToList();
+                    DuplicateForCollections(ref typeOptions);
+                    if (SmartValue.AdditionalTypes != null)
+                        typeOptions.AddRange(SmartValue.AdditionalTypes.Select(x => x.Type).Where(x => x != null));
+                    typeOptions = typeOptions.Distinct().ToList();
+                    
+                    var lines = UnityGenericImplementationGenerator.CreateFactoryClass(classNameField.Value, genericTypeDefinition, typeOptions);
+                    if (lines.IsNullOrEmpty())
+                    {
+                        PLog.Warn<MagnusLogger>($"No class was generated, aborting...");
+                        return;
+                    }
+                    
+                    var outPath = Path.Combine(saveLocation.Value, $"{classNameField.Value}.cs");
+                    File.WriteAllLines(outPath, lines);
+        
+                    PLog.Info<MagnusLogger>($"Code created @ {Path.GetFullPath(outPath)}");
+                })
+                .Show();
+        }
+
+        private void DuplicateForCollections(ref List<Type> typeOptions)
+        {
+            var singleTypes = typeOptions.Where(x => !x.IsArray && !x.ImplementsOpenGenericClass(typeof(List<>))).ToArray();
+            foreach (var singleType in singleTypes)
+            {
+                var arrayType = singleType.MakeArrayType();
+                if (!typeOptions.Contains(arrayType))
+                    typeOptions.Add(arrayType);
+
+                var listType = typeof(List<>).MakeGenericType(singleType);
+                if (!typeOptions.Contains(listType))
+                    typeOptions.Add(listType);
+            }
+        }
+
+        private bool ValidateClassName(string potentialClassName)
+        {
+            if (potentialClassName == null)
+                return false;
+
+            if (_classNameValidator == null)
+                _classNameValidator = new Regex("^[a-zA-Z_]{1}[a-zA-Z_0-9]*$");
+
+            return _classNameValidator.IsMatch(potentialClassName);
+        }
+
+        private ICollection<ValueDropdownItem> GetGenericTypeOptions()
+        {
+            return TypeCache.GetTypesWithAttribute<GenericTypeGenerationAttribute>()
+                .Select(x => new ValueDropdownItem(x.GetCSharpName(), x))
+                .ToArray();
+        }
+    }
+}
