f48a3d1435539f945f4bc4b90e30cca6ac01fcbf
GlennRhinox <glenn@rhinox.training>
2024-01-08 17:00:19 +0100

    [MOD] Playerstart to have exposed color and size variables.

--- Scripts/PlayerManagement/PlayerStart.cs
+++ Scripts/PlayerManagement/PlayerStart.cs
@@ -1,17 +1,32 @@
 using System;
 using System.Collections.Generic;
+using Rhinox.GUIUtils.Attributes;
 using Rhinox.Lightspeed;
 using Rhinox.Utilities;
+using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 
 namespace Rhinox.Magnus
 {
+    [SmartFallbackDrawn]
     public class PlayerStart : MonoBehaviour
     {
 #if UNITY_EDITOR
-        private const string OPERATION_NAME = "GameObject/Player Start";
+        [SerializeField] private Color playerStartColor = new Color(0, 0.8f, 0, 1);
         
+        [FoldoutGroup("Proportions")][SerializeField] private float _playerHeight = 1.8f;
+        [FoldoutGroup("Proportions")][SerializeField] private float _headSize = 0.3f;
+        [FoldoutGroup("Proportions")][SerializeField] private float _legToHeightRatio = 0.45f;
+        [FoldoutGroup("Proportions")][SerializeField] private float _torsoWidth = 0.44f;
+        [FoldoutGroup("Proportions")][SerializeField] private float _armToLegLengthRatio = 0.92f;
+        [FoldoutGroup("Proportions")][SerializeField] private float _legToLegHeadRadiusRatio = 0.35f;
+        [FoldoutGroup("Proportions")][SerializeField] private float _legCenterOffset = 1.2f;
+        
+        private const string OPERATION_NAME = "GameObject/Player Start";
+
         [MenuItem(OPERATION_NAME, false, 26)]
         private static void CreatePlayerStart(MenuCommand menuCommand)
         {
@@ -22,55 +37,58 @@ namespace Rhinox.Magnus
                 playerStart.transform.Reset();
             }
         }
-        
+
         [MenuItem(OPERATION_NAME, true)]
         private static bool ValidateCreate()
         {
             return Selection.transforms.Length <= 1;
         }
-        
+
         private void OnDrawGizmos()
         {
+            var matCache = Gizmos.matrix;
             Color oldColor = Gizmos.color;
-            Color playerStartColor = new Color(0, 0.8f, 0, 1);
-            const float playerHeight = 1.8f;
-            
-            Gizmos.color = playerStartColor;
-            // Draw player body
-            const float headSize = 0.3f;
-            Gizmos.DrawWireSphere(transform.TransformPoint(new Vector3(0, playerHeight - (headSize / 2.0f), 0)), (headSize / 2.0f));
+            Gizmos.matrix = transform.localToWorldMatrix;
+            {
+                Gizmos.color = playerStartColor;
 
-            const float legLength = playerHeight * 0.45f;
-            const float torsoLength = playerHeight - (headSize + legLength);
-            const float torsoWidth = 0.44f;
-            Gizmos.DrawWireCube(transform.TransformPoint(new Vector3(0, legLength + torsoLength / 2.0f, 0)), 
-                transform.TransformDirection(new Vector3(torsoWidth, torsoLength,torsoWidth / 2.7f)));
-            
-            // Draw limbs
-            const float legRadius = headSize * 0.35f;
-            const float legOffset = legRadius * 1.2f;
-            GizmosExt.DrawCapsule(transform.TransformPoint(new Vector3(-legOffset, legLength, 0)), 
-                transform.TransformPoint(new Vector3(-legOffset, 0, 0)), playerStartColor, legRadius);
-            
-            GizmosExt.DrawCapsule(transform.TransformPoint(new Vector3(legOffset, legLength, 0)), 
-                transform.TransformPoint(new Vector3(legOffset, 0, 0)), playerStartColor, legRadius);
+                Gizmos.DrawWireSphere(new Vector3(0, _playerHeight - (_headSize / 2.0f), 0), (_headSize / 2.0f));
 
-            const float armLength = legLength * 0.92f;
-            const float armRadius = legRadius;
-            
-            const float armOffset = armRadius + (torsoWidth / 2.0f);
-            
-            GizmosExt.DrawCapsule(transform.TransformPoint(new Vector3(-armOffset,  playerHeight - headSize, 0)), 
-                transform.TransformPoint(new Vector3(-armOffset, (playerHeight - headSize - armLength), 0)), playerStartColor, armRadius);
-            
-            GizmosExt.DrawCapsule(transform.TransformPoint(new Vector3(armOffset, playerHeight - headSize, 0)), 
-                transform.TransformPoint(new Vector3(armOffset, (playerHeight - headSize - armLength), 0)), playerStartColor, armRadius);
-            
-            // Draw bottom gizmo
-            GizmosExt.DrawCircle(transform.position, transform.up, playerStartColor, 0.5f);
-            GizmosExt.DrawArrow(transform.position, transform.forward, playerStartColor);
+                float legLength = _playerHeight * _legToHeightRatio;
+                float torsoLength = _playerHeight - (_headSize + legLength);
+                
+                Gizmos.DrawWireCube(new Vector3(0, legLength + torsoLength / 2.0f, 0),
+                    new Vector3(_torsoWidth, torsoLength, _torsoWidth / 2.7f));
 
-            Gizmos.color = oldColor;
+                // Draw limbs
+                float legRadius = _headSize * _legToLegHeadRadiusRatio;
+                float legOffset = legRadius * _legCenterOffset;
+                GizmosExt.DrawCapsule(new Vector3(-legOffset, legLength, 0),
+                    new Vector3(-legOffset, 0, 0), playerStartColor, legRadius);
+
+                GizmosExt.DrawCapsule(new Vector3(legOffset, legLength, 0),
+                    new Vector3(legOffset, 0, 0), playerStartColor, legRadius);
+
+                float armLength = legLength * _armToLegLengthRatio;
+                float armRadius = legRadius;
+
+                float armOffset = armRadius + (_torsoWidth / 2.0f);
+
+                GizmosExt.DrawCapsule(new Vector3(-armOffset, _playerHeight - _headSize, 0),
+                    new Vector3(-armOffset, (_playerHeight - _headSize - armLength), 0),
+                    playerStartColor, armRadius);
+
+                GizmosExt.DrawCapsule(new Vector3(armOffset, _playerHeight - _headSize, 0),
+                    new Vector3(armOffset, (_playerHeight - _headSize - armLength), 0),
+                    playerStartColor, armRadius);
+
+                // Draw bottom gizmo
+                GizmosExt.DrawCircle(Vector3.zero, transform.up, playerStartColor, 0.5f);
+                GizmosExt.DrawArrow(Vector3.zero, Vector3.forward, playerStartColor);
+
+                Gizmos.color = oldColor;
+            }
+            Gizmos.matrix = matCache;
         }
 #endif
     }
